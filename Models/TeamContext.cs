b1b059e9fbfac02385446edede96e20d7553727b
Austin <108588518+DragonGuyMan@users.noreply.github.com>
2023-11-25 18:05:47 -0500

    Created DbContext class

---
+++ Models/TeamContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FinalProject.Models
+{
+    public class TeamContext : DbContext
+    {
+        public TeamContext(DbContextOptions<TeamContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+        }
+
+        public DbSet<TeamMember> TeamMembers { get; set; }
+        public DbSet<Job> Jobs { get; set; }
+        public DbSet<FaveMovie> FaveMovies { get; set; }
+        public DbSet<FaveSportsPlayer> FaveSportsPlayers { get; set; }
+    }
+}

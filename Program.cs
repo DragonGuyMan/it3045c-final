40547ae5cb5de35977e8763338b8d5dc6abdf998
Austin <108588518+DragonGuyMan@users.noreply.github.com>
2023-11-26 14:06:17 -0500

    Implemented needed configs for startup

--- Program.cs
+++ Program.cs
@@ -1,12 +1,26 @@
+using FinalProject.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddSwaggerDocument();
 
+builder.Services.AddDbContext<TeamContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("TeamContext"))
+);
+
 var app = builder.Build();
 
 app.UseOpenApi();
 app.UseSwaggerUi3();
+
+// scope needed to allow context.Database.Migrate() to work
+var scope = app.Services.CreateScope();
+
+TeamContext context = scope.ServiceProvider.GetRequiredService<TeamContext>();
+
+context.Database.Migrate();
 
 app.UseAuthorization();
 

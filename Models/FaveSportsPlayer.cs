0bbaa0c5d4b776168bdc786f04f40894f7c955eb
Austin <108588518+DragonGuyMan@users.noreply.github.com>
2023-11-26 13:23:59 -0500

    Changed SportName to Sport

--- Models/FaveSportsPlayer.cs
+++ Models/FaveSportsPlayer.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string TeamMemberName { get; set; }
         public string PlayerName { get; set; }
-        public string SportName { get; set; }
+        public string Sport { get; set; }
         public string ChampionshipNum { get; set; }
     }
 }

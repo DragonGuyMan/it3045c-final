46c6a7f132caaa3988d9e9d6a92235028b40bffc
Somyani Ghimire <ghimirsi@mail.uc.edu>
2023-12-02 00:53:04 +0545

    check it out

---
+++ Controllers/FaveSportsPlayersController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FinalProject.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FaveSportsPlayersController : ControllerBase
+    {
+        private readonly TeamContext _context;
+
+        public FaveSportsPlayersController(TeamContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/FaveSportsPlayers
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<FaveSportsPlayer>>> GetFaveSportsPlayers()
+        {
+            return await _context.FaveSportsPlayers.ToListAsync();
+        }
+
+        // GET: api/FaveSportsPlayers/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<FaveSportsPlayer>> GetFaveSportsPlayer(int id)
+        {
+            var faveSportsPlayer = await _context.FaveSportsPlayers.FindAsync(id);
+
+            if (faveSportsPlayer == null)
+            {
+                return NotFound();
+            }
+
+            return faveSportsPlayer;
+        }
+
+        // PUT: api/FaveSportsPlayers/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutFaveSportsPlayer(int id, FaveSportsPlayer faveSportsPlayer)
+        {
+            if (id != faveSportsPlayer.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(faveSportsPlayer).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FaveSportsPlayerExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/FaveSportsPlayers
+        [HttpPost]
+        public async Task<ActionResult<FaveSportsPlayer>> PostFaveSportsPlayer(FaveSportsPlayer faveSportsPlayer)
+        {
+            _context.FaveSportsPlayers.Add(faveSportsPlayer);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetFaveSportsPlayer", new { id = faveSportsPlayer.Id }, faveSportsPlayer);
+        }
+
+        // DELETE: api/FaveSportsPlayers/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteFaveSportsPlayer(int id)
+        {
+            var faveSportsPlayer = await _context.FaveSportsPlayers.FindAsync(id);
+            if (faveSportsPlayer == null)
+            {
+                return NotFound();
+            }
+
+            _context.FaveSportsPlayers.Remove(faveSportsPlayer);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool FaveSportsPlayerExists(int id)
+        {
+            return _context.FaveSportsPlayers.Any(e => e.Id == id);
+        }
+    }
+}

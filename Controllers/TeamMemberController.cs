65a0e8d7fb3f1fd752391f39d051af638860f253
Your Name <kjaamponsah23@gmail.com>
2023-12-02 08:59:48 -0500

    Edited

---
+++ Controllers/TeamMemberController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FinalProject.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TeamMemberController : ControllerBase
+    {
+        private readonly TeamContext _context;
+
+        public TeamMemberController(TeamContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/TeamMember
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TeamMember>>> GetTeamMembers()
+        {
+            return await _context.TeamMembers.ToListAsync();
+        }
+
+        // GET: api/TeamMember/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TeamMember>> GetTeamMember(int id)
+        {
+            var teamMember = await _context.TeamMembers.FindAsync(id);
+
+            if (teamMember == null)
+            {
+                return NotFound();
+            }
+
+            return teamMember;
+        }
+
+        // PUT: api/TeamMember/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTeamMember(int id, TeamMember teamMember)
+        {
+            if (id != teamMember.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(teamMember).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TeamMemberExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/TeamMember
+        [HttpPost]
+        public async Task<ActionResult<TeamMember>> PostTeamMember(TeamMember teamMember)
+        {
+            _context.TeamMembers.Add(teamMember);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTeamMember", new { id = teamMember.Id }, teamMember);
+        }
+
+        // DELETE: api/TeamMember/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTeamMember(int id)
+        {
+            var teamMember = await _context.TeamMembers.FindAsync(id);
+            if (teamMember == null)
+            {
+                return NotFound();
+            }
+
+            _context.TeamMembers.Remove(teamMember);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TeamMemberExists(int id)
+        {
+            return _context.TeamMembers.Any(e => e.Id == id);
+        }
+    }
+}
